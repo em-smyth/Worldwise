2f604dce49b1f60d899cdc8b9164ef4798cd7f59
Emma <em.smyth5@gmail.com>
2023-12-29 16:02:41 +0000

    Add protection to app routes

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,16 +2,13 @@ import Map from "../components/Map";
 import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
 import User from "../components/User";
-import { useAuth } from "../contexts/FakeAuthContext";
 
 function AppLayout() {
-  const { isAuthenticated } = useAuth();
-
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
-      {isAuthenticated && <User />}
+      <User />
     </div>
   );
 }

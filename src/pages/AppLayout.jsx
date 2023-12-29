6f942bb76e9ab1f032eb4eb6b3f2974a6ca038ca
Emma <em.smyth5@gmail.com>
2023-12-29 15:47:22 +0000

    login and logout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,12 +1,17 @@
 import Map from "../components/Map";
 import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
+import User from "../components/User";
+import { useAuth } from "../contexts/FakeAuthContext";
 
 function AppLayout() {
+  const { isAuthenticated } = useAuth();
+
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      {isAuthenticated && <User />}
     </div>
   );
 }

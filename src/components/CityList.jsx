b8de197de1e69292c64294b6c0397ef4c6a25fb8
Emma <em.smyth5@gmail.com>
2024-02-10 17:37:57 +0000

    add message to city list if no cities

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -5,12 +5,18 @@ import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import { useCities } from "../contexts/CitiesContext";
 import Button from "./Button";
+import Message from "./Message";
 
 function CityList() {
   const { cities, isLoading } = useCities();
   const navigate = useNavigate();
 
   if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
 
   function handleClick() {
     navigate("/app");
